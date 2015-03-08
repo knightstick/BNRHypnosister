0b09c11cacab95323c7f3f9993e4635f3ad41f68
Chris Jewell <chrisjohnjewell@gmail.com>
2015-03-09 10:32:38 +1100

    Initial Commit

---
+++ Hypnosister/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hypnosister
+//
+//  Created by Test Mac on 9/03/2015.
+//  Copyright (c) 2015 Chris Jewell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hypnosister/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hypnosister
+//
+//  Created by Test Mac on 9/03/2015.
+//  Copyright (c) 2015 Chris Jewell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
